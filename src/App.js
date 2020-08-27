d4cd0d7b874320b24f4967b17594d5842e084e3d
undefined-procedure <gomez.aita@hotmail.com>
2020-08-27 01:14:40 -0300

    added header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import store from './redux/store'
+import Header from './components/header/Header';
 
 const App = () => {
   return (
     <Provider store={store}>
+      <Header />
       <div className="app">
       </div>
     </Provider>
