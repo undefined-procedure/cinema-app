927d46b33049c9b232401a8dcfab9d4672b6a6d7
undefined-procedure <gomez.aita@hotmail.com>
2020-08-30 17:17:18 -0300

    added slide component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import store from './redux/store'
 import Header from './components/header/Header';
+import Main from './components/main/Main';
 
 const App = () => {
   return (
     <Provider store={store}>
       <Header />
       <div className="app">
+        <Main />
       </div>
     </Provider>
   );
